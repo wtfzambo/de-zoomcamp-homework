10adfe970cb7a1cd869e9e45b075079549805816
wtfzambo <zambelli.federico@gmail.com>
2023-03-03 00:48:20 +0100

    fix: no error if dir is missing

--- week_4/dbt/taxi_rides_ny/models/staging/trips_data_all/stg_trips_data_all__fhv_data.sql
+++ week_4/dbt/taxi_rides_ny/models/staging/trips_data_all/stg_trips_data_all__fhv_data.sql
@@ -2,7 +2,7 @@ with
     source as (select * from {{ source("trips_data_all", "fhv_data") }}),
     renamed as (
         select
-            {{ adapter.quote("dispatching_base_num") }} as banana,
+            {{ adapter.quote("dispatching_base_num") }},
             {{ adapter.quote("pickup_datetime") }},
             {{ adapter.quote("dropOff_datetime") }},
             {{ adapter.quote("PUlocationID") }},
