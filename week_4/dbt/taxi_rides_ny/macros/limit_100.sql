424baff3a58f0da8f96ffb86257c8a198a4bded3
wtfzambo <zambelli.federico@gmail.com>
2023-03-12 19:16:25 +0100

    finish week 4 stuff and add some extra shit to taskfile

--- week_4/dbt/taxi_rides_ny/macros/limit_100.sql
+++
@@ -1,7 +0,0 @@
-{%- macro limit_10000() -%}
-{%- if var('is_test_run', default=True) == 'true' -%}
-
-limit 10000
-
-{%- endif -%}
-{%- endmacro -%}
--- week_4/dbt/taxi_rides_ny/models/staging/trips_data_all/stg_trips_data_all__fhv_data.sql
+++ week_4/dbt/taxi_rides_ny/models/staging/trips_data_all/stg_trips_data_all__fhv_data.sql
@@ -4,18 +4,37 @@ source as (
     select * from {{ source("trips_data_all", "fhv_data") }}
 )
 
-, renamed as (
+, dedupe_rows as (
     select
-        {{ adapter.quote("dispatching_base_num") }}
-        , {{ adapter.quote("pickup_datetime") }}
-        , {{ adapter.quote("dropOff_datetime") }}
-        , {{ adapter.quote("PUlocationID") }}
-        , {{ adapter.quote("DOlocationID") }}
-        , {{ adapter.quote("SR_Flag") }}
-        , {{ adapter.quote("Affiliated_base_number") }}
+        *
+        , row_number() over(partition by pickup_datetime, dropoff_datetime, pulocationid, dolocationid) as rn
 
     from source
+    where
+        pulocationid is not null
+        and pulocationid != 0
+        and dolocationid is not null
+        and dolocationid != 0
 )
 
-select *
-from renamed
+, renamed as (
+    select
+        {{ dbt_utils.generate_surrogate_key([
+            'pickup_datetime',
+            'dropOff_datetime',
+            'PUlocationID',
+            'DOlocationID']) }} as tripid
+        , {{ adapter.quote("dispatching_base_num") }}
+        , {{ adapter.quote("pickup_datetime") }}
+        , {{ adapter.quote("dropOff_datetime") }} as dropoff_datetime
+        , {{ adapter.quote("PUlocationID") }} as pickup_locationid
+        , {{ adapter.quote("DOlocationID") }} as dropoff_locationid
+        , {{ adapter.quote("SR_Flag") }} as sr_flag
+        , {{ adapter.quote("Affiliated_base_number") }} as affiliated_base_number
+
+    from dedupe_rows
+    where rn = 1
+)
+
+select * from renamed
+{{ limit_10000() }}
